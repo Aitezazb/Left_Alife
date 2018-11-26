4bd6f786b62d7c7048cf56a6b0d78de74af1f629
Aitezaz Bilal <aitezazbilal95@gmail.com>
2018-11-27 03:06:37 +0500

    Added - playerground, bullet script

---
+++ Left Alife/Assets/BulletFired.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFired : MonoBehaviour {
+
+    public Transform bulletPerfeb;
+    public Transform BulletPosition;
+
+    public float speed;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform bullet;
+        Vector3 InputMouse;float step = speed * Time.deltaTime;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+            bullet =  Instantiate(bulletPerfeb, BulletPosition.position, Quaternion.identity);
+            //Debug.Log(Input.mousePosition);
+            InputMouse = Input.mousePosition;
+            
+        }
+        
+    }
+}
